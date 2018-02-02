9d15029dee8b6517f503aacc69f556531807bb1f
José Domingo <josedom24@gmail.com>
2018-02-02 11:15:41 +0100

    python3

--- xpath.py
+++ xpath.py
@@ -10,21 +10,21 @@ contenido=contenido[:-1]
 doc = etree.parse(contenido)
 print ("'q' for exit...")
 
-consulta=raw_input("XPATH:")
+consulta=input("XPATH:")
 while consulta!="q":
 	try:
 		res=doc.xpath(consulta)
 	except:
-		print "Error en consulta XPATH."
+		print ("Error en consulta XPATH.")
 		res=[]
 	if isinstance(res,list):	
 		for r in res:
 			if isinstance(r,etree._Element):
-				print etree.tostring(r,pretty_print=True)
+				print (etree.tostring(r,pretty_print=True).decode("utf-8") )
 			else:
-				print r
+				print (str(r.decode("utf-8")))
 	else:
-		print res
-	consulta=raw_input("XPATH:")
+		print (str(res.decode("utf-8")))
+	consulta=input("XPATH:")
 
 
